369d440b9cb4c79a84cbe7db870468018355a0de
fbzr <fabricio.spb@gmail.com>
2020-05-04 11:14:14 -0700

    fix index.js app not defined to server

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const server = require('./server');
 // DEPLOYMENT - Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static   
-    app.use(express.static('client/build'));
+    server.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
+    server.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
