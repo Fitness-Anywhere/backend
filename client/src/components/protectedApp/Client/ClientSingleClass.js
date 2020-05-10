8d61d5f31d91690b2f6b01baed7d2712739be0c7
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-10 15:59:56 -0700

    added homepage, fixed login/sign up bugs, changed form styles, form route works as expected.

--- client/src/components/protectedApp/Client/ClientSingleClass.js
+++ client/src/components/protectedApp/Client/ClientSingleClass.js
@@ -15,79 +15,81 @@ const ClientSingleClass = () => {
   const fetchClass = async () => {
     const res = await axiosWithAuth().get(`/api/classes/${c_id}`);
     setCurrentClass(res.data);
-  }
+  };
 
   useEffect(() => {
     dispatch({ type: "PROCCESSING_PAYMENT" });
-    
+
     fetchClass();
   }, []);
 
   return (
-    <> { currentClass && (
-      <>    
-        <Sharednav />
-        <div className="InstructorSingleClass">
-          <div className="wrapper-class">
-            <div className="InstructorSingleClass-wrapper">
-              <div className="img-wrapper">
-                <img src={currentClass.image_url} alt={currentClass.name} />
+    <>
+      {" "}
+      {currentClass && (
+        <>
+          <Sharednav />
+          <div className="InstructorSingleClass">
+            <div className="wrapper-class">
+              <div className="InstructorSingleClass-wrapper">
+                <div className="img-wrapper">
+                  <img src={currentClass.image_url} alt={currentClass.name} />
+                </div>
+                <div className="right-side">
+                  <h2 className="name">{currentClass.name}</h2>
+                  <p className="address">
+                    Location:
+                    <span>{currentClass.location}</span>
+                  </p>
+                  <p className="intensity">
+                    Intensity:
+                    <span>{currentClass.intensity}</span>
+                  </p>
+                  <p className="max-size">
+                    Class Size:
+                    <span>{currentClass.max_class_size}</span>
+                  </p>
+                  <p className="price">
+                    Price:
+                    <span>${currentClass.price}</span>
+                  </p>
+                  <p className="start">
+                    Starts at:
+                    <span>{currentClass.start_time}</span>
+                  </p>
+                  <p className="type">
+                    Class type:
+                    <span>{currentClass.type}</span>
+                  </p>
+                  <p className="duration">
+                    Class duration:
+                    <span>{currentClass.duration} mins</span>
+                  </p>
+                </div>
               </div>
-              <div className="right-side">
-                <h2 className="name">
-                  {currentClass.name}
-                </h2>
-                <p className="address">
-                  Location:
-                  <span>{currentClass.location}</span>
-                </p>
-                <p className="intensity">
-                  Intensity:
-                  <span>{currentClass.intensity}</span>
-                </p>
-                <p className="max-size">
-                  Class Size:
-                  <span>{currentClass.max_class_size}</span>
-                </p>
-                <p className="price">
-                  Price:
-                  <span>${currentClass.price}</span>
-                </p>
-                <p className="start">
-                  Starts at:
-                  <span>{currentClass.start_time}</span>
-                </p>
-                <p className="type">
-                  Class type:
-                  <span>{currentClass.type}</span>
-                </p>
-                <p className="duration">
-                  Class duration:
-                  <span>{currentClass.duration} mins</span>
-                </p>
-              </div>
-            </div>
-            <div className="bottom-description">
-              <div className="InstructorSingleClass-description">
-                <h4>description</h4>
-                <p>{currentClass.description}</p>
-              </div>
-              <div id="buy-btn">
-                <ClientModel data={currentClass} />
+              <div className="bottom-description">
+                <div className="InstructorSingleClass-description">
+                  <h4>description</h4>
+                  <p>{currentClass.description}</p>
+                </div>
+                <div id="buy-btn">
+                  <ClientModel data={currentClass} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="googleMap-wrapper">
-          <GoogleMap location={currentClass.location} />
-        </div>
-        {/* <div className="goback-btn">
+          <div className="googleMap-wrapper">
+            {/**
+       <GoogleMap location={currentClass.location} />
+      */}
+          </div>
+          {/* <div className="goback-btn">
           <Link to={`/account/client/${id}`}>Go back</Link>
         </div> */}
-      </> 
-    )}
+        </>
+      )}
     </>
-  )
+  );
 };
 
 export default ClientSingleClass;
